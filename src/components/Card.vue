adb93469461540888c0392fd54d47d281056aaa9
ethan <e19920526@gmail.com>
2025-12-11 20:30:37 +0800

    add Card layout

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,56 +1,20 @@
 <script setup>
 import { ref } from 'vue';
-const props = defineProps(['pokemon'])
-const types = ref('')
+const props = defineProps(['pokemon']);
 
 </script>
 
 <template>
-    <div class="row row-cols-md-4 g-4">
-        <div class="col">
-            <div class="card">
-                <img :src="props.pokemon.image" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">名字: {{ props.pokemon.name }}</h5>
-                    <h5 class="card-title">屬性: {{ props.pokemon.types.join('、') }}</h5>
-                    <h5 class="card-title">編號: {{ props.pokemon.number }}</h5>
-                </div>
+    <div class="col">
+        <div class="card border-primary border-opacity-50 border-2 rounded">
+            <img :src="props.pokemon.image" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">名字: {{ props.pokemon.name }}</h5>
+                <h5 class="card-title">屬性: {{ props.pokemon.types.join('、') }}</h5>
+                <h5 class="card-title">編號: {{ props.pokemon.number }}</h5>
             </div>
         </div>
-        <!-- <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-        </div> -->
-        <!-- <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-        </div> -->
     </div>
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/CardList.vue
+++ src/views/CardList.vue
@@ -1,29 +1,14 @@
 <script setup>
 import Card from '@/components/Card.vue';
 import pokemonData from '@/data/pokemon.json'
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 const pokemonList = ref(pokemonData)
-// console.log(pokemon.value.name)
-// for (const p in pokemonData) {
-//     console.log(p)
-// }
-
-
-// console.log(pokemonData[0].name)
 </script>
 
 <template>
-    <!-- <div v-for="(mon, index) in pokemon" :key="index">
-        <Card item="mon">{{ pokemon }}</Card>
-    </div> -->
-    <div v-for="(pokemon, key) in pokemonList" :key="pokemon.number">
-        <!-- <p>{{ typeof(item.number) }}</p>
-        <p>{{ typeof(item.image) }}</p>
-        <p>{{ typeof(item.name) }}</p>
-        <p>{{ item.types }}</p> -->
-        <Card :pokemon="pokemon"></Card>
+    <div class="row row-cols-1 row-cols-md-5 g-4">
+        <Card v-for="(pokemon, key) in pokemonList" :key="pokemon.number" :pokemon="pokemon"></Card>
     </div>
-
 </template>
 
 <style scoped></style>
